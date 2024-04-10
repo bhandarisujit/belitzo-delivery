9edac62229c583bef314bfdfee09190c1f53f724
shlok <shlok.pardeshi@gmail.com>
2024-04-10 21:11:31 +0530

    all

---
+++ .history/lib/main_20231011221335.dart
@@ -0,0 +1,156 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:efood_multivendor_driver/controller/localization_controller.dart';
+import 'package:efood_multivendor_driver/controller/splash_controller.dart';
+import 'package:efood_multivendor_driver/controller/theme_controller.dart';
+import 'package:efood_multivendor_driver/data/model/body/notification_body.dart';
+import 'package:efood_multivendor_driver/helper/notification_helper.dart';
+import 'package:efood_multivendor_driver/helper/route_helper.dart';
+import 'package:efood_multivendor_driver/theme/dark_theme.dart';
+import 'package:efood_multivendor_driver/theme/light_theme.dart';
+import 'package:efood_multivendor_driver/util/app_constants.dart';
+import 'package:efood_multivendor_driver/util/messages.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:get/get.dart';
+import 'package:url_strategy/url_strategy.dart';
+import 'controller/auth_controller.dart';
+import 'helper/get_di.dart' as di;
+import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:workmanager/workmanager.dart';
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+Future<void> main() async {
+  if(!GetPlatform.isWeb) {
+    HttpOverrides.global = new MyHttpOverrides();
+  }
+  setPathUrlStrategy();
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  Map<String, Map<String, String>> _languages = await di.init();
+
+  NotificationBody _body;
+  try {
+    if (GetPlatform.isMobile) {
+      final RemoteMessage remoteMessage = await FirebaseMessaging.instance.getInitialMessage();
+      if(remoteMessage != null){
+        _body = NotificationHelper.convertNotification(remoteMessage.data);
+      }
+      await NotificationHelper.initialize(flutterLocalNotificationsPlugin);
+      FirebaseMessaging.onBackgroundMessage(myBackgroundMessageHandler);
+    }
+  }catch(e) {}
+
+  runApp(MyApp(languages: _languages, body: _body));
+}
+
+const simpleTaskKey = "be.tramckrijte.workmanagerExample.simpleTask";
+const rescheduledTaskKey = "be.tramckrijte.workmanagerExample.rescheduledTask";
+const failedTaskKey = "be.tramckrijte.workmanagerExample.failedTask";
+const simpleDelayedTask = "be.tramckrijte.workmanagerExample.simpleDelayedTask";
+const simplePeriodicTask =
+    "be.tramckrijte.workmanagerExample.simplePeriodicTask";
+const simplePeriodic1HourTask =
+    "be.tramckrijte.workmanagerExample.simplePeriodic1HourTask";
+
+@pragma(
+    'vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
+    switch (task) {
+      case simpleTaskKey:
+        print("$simpleTaskKey was executed. inputData = $inputData");
+        final prefs = await SharedPreferences.getInstance();
+        prefs.setBool("test", true);
+        print("Bool from prefs: ${prefs.getBool("test")}");
+        break;
+      case rescheduledTaskKey:
+        final key = inputData!['key']!;
+        final prefs = await SharedPreferences.getInstance();
+        if (prefs.containsKey('unique-$key')) {
+          print('has been running before, task is successful');
+          return true;
+        } else {
+          await prefs.setBool('unique-$key', true);
+          print('reschedule task');
+          return false;
+        }
+      case failedTaskKey:
+        print('failed task');
+        return Future.error('failed');
+      case simpleDelayedTask:
+        print("$simpleDelayedTask was executed");
+        break;
+      case simplePeriodicTask:
+        print("$simplePeriodicTask was executed");
+        break;
+      case simplePeriodic1HourTask:
+        print("$simplePeriodic1HourTask was executed");
+        break;
+      case Workmanager.iOSBackgroundTask:
+        print("The iOS background fetch was triggered");
+        Directory? tempDir = await getTemporaryDirectory();
+        String? tempPath = tempDir.path;
+        print(
+            "You can access other plugins in the background, for example Directory.getTemporaryDirectory(): $tempPath");
+        break;
+    }
+
+    return Future.value(true);
+  });
+}
+
+class MyApp extends StatelessWidget {
+  final Map<String, Map<String, String>> languages;
+  final NotificationBody body;
+  MyApp({@required this.languages, @required this.body});
+
+  void _route() {
+    Get.find<SplashController>().getConfigData().then((bool isSuccess) async {
+      if (isSuccess) {
+        if (Get.find<AuthController>().isLoggedIn()) {
+          Get.find<AuthController>().updateToken();
+        }
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if(GetPlatform.isWeb) {
+      Get.find<SplashController>().initSharedData();
+      _route();
+    }
+
+    return GetBuilder<ThemeController>(builder: (themeController) {
+      return GetBuilder<LocalizationController>(builder: (localizeController) {
+        return GetBuilder<SplashController>(builder: (splashController) {
+          return (GetPlatform.isWeb && splashController.configModel == null) ? SizedBox() : GetMaterialApp(
+            title: AppConstants.APP_NAME,
+            debugShowCheckedModeBanner: false,
+            navigatorKey: Get.key,
+            theme: themeController.darkTheme ? dark : light,
+            locale: localizeController.locale,
+            translations: Messages(languages: languages),
+            fallbackLocale: Locale(AppConstants.languages[0].languageCode, AppConstants.languages[0].countryCode),
+            initialRoute: RouteHelper.getSplashRoute(body),
+            getPages: RouteHelper.routes,
+            defaultTransition: Transition.topLevel,
+            transitionDuration: Duration(milliseconds: 500),
+          );
+        });
+      });
+    });
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext context) {
+    return super.createHttpClient(context)..badCertificateCallback = (X509Certificate cert, String host, int port) => true;
+  }
+}
